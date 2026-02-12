ab8c1626b33ea39dc4fcc7c0c9843344d9ccba54
Bonzokoles <bonzokoles@users.noreply.github.com>
2026-02-12 16:24:41 +0100

    Update D1 Types and Env

--- src/env.d.ts
+++ src/env.d.ts
@@ -14,6 +14,31 @@ interface ImportMetaEnv {
   readonly NODE_ENV?: string;
 }
 
+
 interface ImportMeta {
   readonly env: ImportMetaEnv;
+}
+
+/// <reference types="@astrojs/cloudflare" />
+type D1Database = import("@cloudflare/workers-types").D1Database;
+type KVNamespace = import("@cloudflare/workers-types").KVNamespace;
+
+declare namespace App {
+  interface Locals extends Record<string, any> {
+    runtime: {
+      env: {
+        DB: D1Database;
+        ANALYTICS_DB: D1Database;
+        SESSION: KVNamespace;
+        CACHE: KVNamespace;
+        ANALYTICS_KV: KVNamespace;
+        AI: any;
+      };
+      cf: Record<string, any>;
+      ctx: {
+        waitUntil: (promise: Promise<any>) => void;
+        passThroughOnException: () => void;
+      };
+    };
+  }
 }
